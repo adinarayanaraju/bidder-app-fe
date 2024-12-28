c1724f15244d2779d7e30664783077c42d237672
apni-coding <apnicoding72@gmail.com>
2024-12-28 11:09:54 +0530

    add formik for form validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Button } from 'reactstrap';
 import { useEffect } from 'react';
 import { fetchTestData } from './redux/slices/testSlice';
+import Signup from './views/auth/Signup';
 function App() {
   const dispatch = useDispatch();
   const {data, isLoading, error} = useSelector((state)=> state.test)
@@ -12,7 +13,7 @@ function App() {
   },[])
   return (
    <div className='mt-4'>
-    <Button>Click me</Button>
+    <Signup />
    </div>
   );
 }
