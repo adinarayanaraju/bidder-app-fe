80616fbf544a75192ca3f88a353fe2b5b7778010
apni-coding <apnicoding72@gmail.com>
2025-04-18 09:08:49 +0530

    auction photo upload ui

---
+++ src/views/createAuction/components/AuctionPhotos.js
@@ -0,0 +1,54 @@
+import React, { useRef } from "react";
+import { Label } from "reactstrap";
+import uploadIcon from "../../../assets/icons/upload.svg";
+import deleteIcon from "../../../assets/icons/white_delete.svg";
+import dummyImage from "../../../assets/icons/warning.svg";
+export default function AuctionPhotos() {
+  const fileInputRef = useRef(null);
+
+  const handleFileClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    console.log(e.target.files);
+  };
+  return (
+    <div className="auction-photos-wrapper">
+      <Label className="form-label">
+        Add product photos (min 3)
+        <span className="text-danger ms-1">*</span>
+      </Label>
+      <div className="image-wrapper d-flex gap-4 flex-wrap">
+        <div className="upload-file">
+          <div className="upload-box" onClick={handleFileClick}>
+            <img src={uploadIcon} alt="" />
+            <p>Upload a photo</p>
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              hidden
+              ref={fileInputRef}
+              accept=".jpg,.jpeg,.png"
+            />
+          </div>
+          <div className="photo-validation mt-2">
+            <p className="max-size m-0 mb-1">Max Size: 25MB</p>
+            <p className="allow-info m-0">JPG, PNG only</p>
+          </div>
+        </div>
+        <div className="single-photo">
+          <div className="image-container">
+            <img src={dummyImage} alt="" className="main-image" />
+            <div className="delete-icon">
+              <img src={deleteIcon} alt="" />
+            </div>
+          </div>
+          <p className="single-photo-name m-0 mb-1 ">xyz name.jpg</p>
+          <p className="single-photo-size m-0">24MB</p>
+        </div>
+      </div>
+    </div>
+  );
+}
