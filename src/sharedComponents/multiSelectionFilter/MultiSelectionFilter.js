4427c1af46fcac5ff2fe97e5b27288f4361d4f70
apni-coding <apnicoding72@gmail.com>
2025-05-03 23:55:13 +0530

    add a custom multiselection filter dropdown

---
+++ src/sharedComponents/multiSelectionFilter/MultiSelectionFilter.js
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from "react";
+import "./multiSelectionFilter.scss";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Input,
+  Button,
+} from "reactstrap";
+
+export default function MultiSelectionFilter({
+  label,
+  options = [], //{label, value}
+  value = [],
+  onApply,
+  disabled = false,
+}) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selected, setSelected] = useState(value);
+
+  useEffect(() => {
+    setSelected(value);
+  }, [value, dropdownOpen]);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  const handleChange = (option) => {
+    const exists = selected?.some((item) => item?.value === option?.value);
+    if (exists) {
+      setSelected(selected?.filter((item) => item?.value !== option?.value));
+    } else {
+      setSelected([...selected, option]);
+    }
+  };
+
+  const handleSelectAllToggle = () => {
+    if (selected?.length === options?.length) {
+      setSelected([]); //Deselect
+    } else {
+      setSelected([...options]); //Select all
+    }
+  };
+
+  const handleApply = () => {
+    onApply?.(selected);
+    setDropdownOpen(false);
+  };
+
+  const handleReset = () => {
+    setSelected([]);
+  };
+
+  const isSelected = (option) => {
+    return true;
+  };
+
+  const isAllSelected = selected?.length === options?.length;
+  
+  return (
+    <>
+      <Dropdown
+        isOpen={dropdownOpen}
+        toggle={toggle}
+        className="filter-dropdown-wrapper"
+      >
+        <DropdownToggle caret disabled={disabled} color="light">
+          {label}
+          {value?.length > 0 && (
+            <span className="selected-filter-counter">{value?.length}</span>
+          )}
+        </DropdownToggle>
+        <DropdownMenu className="filter-dropdown-men">
+          <div className="filter-list-wrapper">
+            <div className="form-check d-flex align-items-center cursor-pointer">
+              <Input
+                type="checkbox"
+                className="form-check-input me-2"
+                id={"selectAll"}
+                disabled={disabled}
+                onChange={handleSelectAllToggle}
+                checked={isAllSelected}
+              />
+              <label htmlFor={option?.value}>{option?.label}</label>
+            </div>
+            <div className="checkbox-list">
+              {options?.map((option) => (
+                <DropdownItem
+                  className="px-0 drop-down-item"
+                  key={option?.value}
+                >
+                  <div className="form-check d-flex align-items-center">
+                    <Input
+                      type="checkbox"
+                      className="form-check-input me-2"
+                      id={option?.value}
+                      disabled={disabled}
+                      onChange={() => handleChange(option)}
+                      checked={isSelected(option)}
+                    />
+                    <label htmlFor={option?.value}>{option?.label}</label>
+                  </div>
+                </DropdownItem>
+              ))}
+            </div>
+          </div>
+
+          <div className="filter-btn-wrapper d-flex justify-content-between mt-3 gap-2">
+            <Button size="sm" className="reset-btn" onClick={handleReset}>
+              Reset
+            </Button>
+            <Button size="sm" className="apply-btn" onClick={handleApply}>
+              Apply
+            </Button>
+          </div>
+        </DropdownMenu>
+      </Dropdown>
+    </>
+  );
+}
